3075a301aaaa9cac1f3c4c908d14f81f5049d96f
Dakota Smith <73180360+twofoldtech-dakota@users.noreply.github.com>
2023-02-10 15:53:46 -0600

    stats

--- components/stats.tsx
+++ components/stats.tsx
@@ -1,11 +1,29 @@
+const stats = [
+  { name: "Total Subscribers", stat: "71,897" },
+  { name: "Avg. Open Rate", stat: "58.16%" },
+  { name: "Avg. Click Rate", stat: "24.57%" },
+];
 export default function Stats() {
   return (
-    <section className="mx-auto flex w-full items-center justify-between px-10">
-      <div className="overflow-hidden rounded-xl border bg-white">
-        <div className="h-32 w-32 ">
-          <div className="ml-2 h-full w-full bg-black"></div>
-        </div>
-      </div>
-    </section>
+    <div className="mx-auto flex w-full items-center justify-between bg-lighter_black  px-10">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">
+        Last 30 days
+      </h3>
+      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        {stats.map((item) => (
+          <div
+            key={item.name}
+            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+          >
+            <dt className="truncate text-sm font-medium text-gray-500">
+              {item.name}
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              {item.stat}
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
   );
 }
